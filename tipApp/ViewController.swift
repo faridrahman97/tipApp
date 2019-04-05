b41b640a25740f11a15c814775c0c00024b044a9
Farid Rahman <beatsbyfar.id@gmail.com>
2019-04-05 00:25:20 -0700

    Initial Commit

---
+++ tipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipApp
+//
+//  Created by Farid Rahman on 4/5/19.
+//  Copyright © 2019 Farid Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
